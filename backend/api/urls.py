4a6bcfe20c0086a83b57be383974e8016f504ff7
LoayAbassi <abassi.loay@gmail.com>
2024-09-10 15:24:06 +0100

    post details view, post view_count established.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,12 +3,16 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from api import views as api_views
 
 urlpatterns = [
+    # acess token
     path("user/token/", api_views.MyTokenObtainPairView.as_view()),
     path("user/token/refresh/", TokenRefreshView.as_view()),
+    # user
     path("user/register/", api_views.RegisterView.as_view()),
     path("user/profile/<user_id>/", api_views.ProfileView.as_view()),
+    # post
     path("post/category/list/",api_views.CategoryListAPIView.as_view()),
     path("post/category/posts/<category_slug>",api_views.PostCategoryListAPIView.as_view()),
-
+    path("post/lists/",api_views.PostListAPIView.as_view()),
+    path("post/detail/<slug>",api_views.PostDetailAPIView.as_view() )
 
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -67,3 +67,26 @@ class PostCategoryListAPIView(generics.ListAPIView):
         posts = api_models.Post.objects.filter(
             category=category, status="Active")
         return posts
+
+
+class PostListAPIView(generics.ListAPIView):
+    serializer_class = api_serializer.PostSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        return api_models.Post.objects.filter(status="Active")
+
+
+class PostDetailAPIView(generics.RetrieveAPIView):
+    serializer_class = api_serializer.PostSerializer
+    permission_classes = [AllowAny]
+
+    def get_object(self):
+        slug = self.kwargs['slug']
+        post = api_models.Post.objects.get(slug=slug, status="Active")
+        post.view = post.view + 1
+        post.save()
+
+        return post
+
+
